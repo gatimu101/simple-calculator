9fbc51b564254d5c47e8e2851bde498dc521dcdd
gatimu101 <98744095+gatimu101@users.noreply.github.com>
2022-02-14 16:32:58 +0300

    Add files via upload

---
+++ irene gatimu.c
@@ -0,0 +1,30 @@
+/*
+    simple calculator by irene gatimu
+    14th feb 2022
+    MIT lisence
+  /'  c99 compiler
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char name[100];
+    int n1,n2,sum,diff,product;
+    float quot;
+    printf{"simple calculator/n"};
+    printf{"enter your name: "};
+    scanf("%d%d",&n1,&n2);
+
+    sum+n1+n2;
+    diff=n1-n2
+    product=n1*n2;
+    quot={float}n1/n2;
+
+    prinf{"Hey %s, here is your results:/n",name};
+    printf{"%d + %d=%d/n",n1,n2,sum};
+    printf{"%d - %d=%d/n",n1,n2,diff};
+    printf{"%d * %d=%d/n",n1,n2,product};
+    printf{"%d / %d=%d/n",n1,n2,quot};
+}
